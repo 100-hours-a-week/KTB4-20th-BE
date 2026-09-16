7f2a15b07cb406cee2c2187782bda00dbeb28fb7
rndnjsgud <00kwh@naver.com>
2026-09-16 16:13:32 +0900

    chore: initialize PlanIt backend

---
+++ src/main/java/com/planit/PlanitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.planit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanitBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanitBackendApplication.class, args);
+	}
+
+}
