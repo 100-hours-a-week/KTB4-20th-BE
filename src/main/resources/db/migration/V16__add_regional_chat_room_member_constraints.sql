1c35e102e5024ca2ad92092e0ff85ff35e254727
rndnjsgud <00kwh@naver.com>
2026-09-22 16:11:57 +0900

    fix: flyway 버전 충돌 오류 해결

--- src/main/resources/db/migration/V16__add_regional_chat_room_member_constraints.sql
+++
@@ -1,8 +0,0 @@
-UPDATE regional_chat_room_members
-SET joined_at = CURRENT_TIMESTAMP(6)
-WHERE joined_at IS NULL;
-
-ALTER TABLE regional_chat_room_members
-    MODIFY joined_at DATETIME(6) NOT NULL,
-    ADD CONSTRAINT UK_REGIONAL_CHAT_ROOM_MEMBERS_USER_ROOM
-        UNIQUE (user_id, regional_chat_room_id);
